4913e21e43aa44a40ecc201db403c3cdbeeb9e50
devendra-devarakonda <devendra-devarakonda>
2024-10-11 18:20:25 +0530

    updated changes

--- My_portfolio/script.js
+++ My_portfolio/script.js
@@ -1,12 +1,19 @@
-let next = document.querySelector('.next')
-let prev = document.querySelector('.prev')
+// let next = document.querySelector('.next')
+// let prev = document.querySelector('.prev')
 
-next.addEventListener('click', function(){
-    let items = document.querySelectorAll('.item')
-    document.querySelector('.slide').appendChild(items[0])
-})
+// next.addEventListener('click', function(){
+//     let items = document.querySelectorAll('.item')
+//     document.querySelector('.slide').appendChild(items[0])
+// })
 
-prev.addEventListener('click', function(){
-    let items = document.querySelectorAll('.item')
-    document.querySelector('.slide').prepend(items[items.length - 1]) // here the length of items = 6
-})
+// prev.addEventListener('click', function(){
+//     let items = document.querySelectorAll('.item')
+//     document.querySelector('.slide').prepend(items[items.length - 1]) // here the length of items = 6
+// })
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x')
+    navbar.classList.toggle('active');
+}
